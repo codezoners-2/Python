38d49bcbc57bf8e8481eba733fcc13087eae7754
theodoros <marinero@gmail.com>
2015-03-09 10:03:54 +0000

    / ‘03_regular_expressions/code-examples/bracketsForEmail_DEMO.py’

--- 03_regular_expressions/code-examples/bracketsForEmail_DEMO.py
+++ 03_regular_expressions/code-examples/bracketsForEmail_DEMO.py
@@ -3,7 +3,7 @@ import re
 email = 'email codezoners-2@rave.ac.uk for help'
 
 ## find email address
-match = re.search(r'[\w.\d-]*@[.\w ]*', email)
+match = re.search(r'[\w.\d-]*@[.\w]*', email)
 
 if match: print 'example 1 -> I found: ', match.group()
 else: print 'example 1 -> not found'
