870d68749a85911e0e61d7d7462d990d6bb1cb39
theodoros <marinero@gmail.com>
2015-03-09 09:43:11 +0000

    / ‘03_regular_expressions/code-examples/codezoners_DEMO.py’

--- 03_regular_expressions/code-examples/codezoners_DEMO.py
+++ 03_regular_expressions/code-examples/codezoners_DEMO.py
@@ -16,7 +16,7 @@ if match: print 'example 2 -> I found: ', match.group()
 else: print 'example 2 -> not found'
 
 ## use \d to locate the digit specifically
-match = re.search(r'codezone\d', myString)
+match = re.search(r'code\tzone\d', myString)
 if match: print 'example 3 -> I found: ', match.group()
 else: print 'example 3 -> not found'
 
