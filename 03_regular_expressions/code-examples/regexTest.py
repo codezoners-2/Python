134dc11f453d41460ce19e117a89a72e15013925
theodoros <marinero@gmail.com>
2015-03-09 09:35:12 +0000

    / ‘03_regular_expressions/code-examples/regexTest.py’

--- 03_regular_expressions/code-examples/regexTest.py
+++ 03_regular_expressions/code-examples/regexTest.py
@@ -2,6 +2,11 @@ import re
 
 myString = "I love python!"
 
-patt = r'love \w\w\w\w\w\w'
+patt = 'love \w\w\w\w\w\w'
 
 match = re.search(patt, myString)
+
+if match:
+	print match.group()
+else:
+	print "I found no matches"
