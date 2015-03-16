a64520ac12a8c7dea06c7c403cfe8e466a6da0ed
theodoros <marinero@gmail.com>
2015-03-16 09:33:35 +0000

    + ‘03_regular_expressions/assignments/stateStats/WORKED/stateStats/data/landsize.html’ + ‘03_regular_expressions/assignments/stateStats/WORKED/stateStats/data/originalDataFile.html’ + ‘03_regular_expressions/assignments/stateStats/WORKED/stateStats/data/population.html’ + ‘03_regular_expressions/assignments/stateStats/WORKED/stateStats/data/usMap.svg’ + ‘03_regular_expressions/assignments/stateStats/WORKED/stateStats/dummyFile.txt’ + ‘03_regular_expressions/assignments/stateStats/WORKED/stateStats/logic.py’ + ‘03_regular_expressions/assignments/stateStats/WORKED/stateStats/stateStats.pyde’ + ‘03_regular_expressions/assignments/stateStatsImproved/HANDED/stateStatsImproved/README.md’ + ‘03_regular_expressions/assignments/stateStatsImproved/HANDED/stateStatsImproved/data/landsize.html’ + ‘03_regular_expressions/assignments/stateStatsImproved/HANDED/stateStatsImproved/data/originalDataFile.html’ ...

---
+++ 03_regular_expressions/assignments/stateStatsImproved/HANDED/stateStatsImproved/logic.py
@@ -0,0 +1,65 @@
+import re
+import os
+
+# returns state name from string			
+def extractData(aString):
+	'''	
+	write test here
+	
+	'''
+	return 0
+
+# reads a file and returns a list
+def fileToString(filename):
+	'''
+	>>> fileToString('dummyFile.txt')
+	'one\\ntwo\\n'
+	'''
+	f = open(filename)
+	contentsList = f.read()
+	f.close()
+	return contentsList
+
+
+def getMaxValue(aDict):
+	'''
+	>>> getMaxValue({'vm': ['vermont', 100], 'ak': ['alaska', 5]})
+	100
+	
+	'''
+	maxValue = 0
+	for state in aDict.values():
+		if state[1]>maxValue:
+			maxValue=state[1]
+	return maxValue
+
+def getMinValue(aDict):
+	'''
+	>>> getMinValue({'vm': ['vermont', 100], 'ak': ['alaska', 5]})
+	5
+	
+	'''
+	minValue = 100000000
+	for state in aDict.values():
+		if state[1]<minValue:
+			minValue=state[1]
+	return minValue
+
+# main function is called only when program is run
+# from command line
+def main():
+	print "*** POPULATION DATA ***"
+	fileWithPath = os.path.join("data", "population.html")
+	myDict = extractData(fileToString(fileWithPath))
+	if myDict:
+		for e in myDict: print '{0:5} {1:18} {2}'.format(e, myDict[e][0], myDict[e][1])
+	
+	print 
+	print "*** LAND SIZE DATA ***"	
+	fileWithPath = os.path.join("data", "landsize.html")
+	myDict = extractData(fileToString(fileWithPath))
+	if myDict:
+		for e in myDict: print '{0:5} {1:18} {2}'.format(e, myDict[e][0], myDict[e][1])
+		
+if __name__ == "__main__":
+	main()
--- 03_regular_expressions/code-examples/group_and_findall_DEMO.py
+++ 03_regular_expressions/code-examples/group_and_findall_DEMO.py
@@ -3,7 +3,7 @@ import re
 email = ''' email codezoners-2@mydomain.com for help or contact@mydomain.com'''
 
 ## find all email addresses
-match = re.findall(r'________________', email)
+match = re.findall(r'([\w-]+)@([\w.-]+)', email)
 
 for m in match:		# results come back as a list of tuples
 	print m		    # a single tuple
